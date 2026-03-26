22be51b3cb07323aa997fc5f949990a3640f0c13
Amit Ray <amitray@lawsikho.in>
2026-03-26 23:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -968,7 +968,7 @@ gen_static  = st.sidebar.button("📅 Generate Duration Report")
 st.sidebar.divider()
 st.sidebar.markdown("""
 <div style='font-size:.72rem; color:var(--text-muted,#6B7280); font-weight:500; letter-spacing:0.3px;'>
-    Designed By: <b>Amit Ray</b><br>
+    DESIGNED BY: <b>AMIT RAY</b><br>
     <a href="mailto:amitray@lawsikho.com" style="color:var(--accent-primary); text-decoration:none;">amitray@lawsikho.com</a>
 </div>
 """, unsafe_allow_html=True)
