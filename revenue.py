402e92528974b3de3e7dcdfb05479cb9fa5a2e43
Amit Ray <amitray@lawsikho.in>
2026-03-29 16:09:55 +0530

    Update revenue.py

--- revenue.py
+++ revenue.py
@@ -853,7 +853,7 @@ with tab1:
                         ("Bootcamp-Collection Revenue",              fmt_inr(metrics['collection_rev']), "🏦"),
                         ("Community Revenue (Direct + Collection)",               fmt_inr(metrics['community_rev']),  "🌐"),
                         ("Direct/Other Revenue (INCL. Funnel)",                  fmt_inr(metrics['direct_rev']),     "🎯"),
-                        ("Details Not Available/ Updated Yet",           fmt_inr(metrics['dna_rev']),        "❓"),
+                        ("Details Not Available/ Not Updated Yet",           fmt_inr(metrics['dna_rev']),        "❓"),
                     ]
 
                     cols = st.columns(len(kpis))
