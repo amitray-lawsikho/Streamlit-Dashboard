eda8a37a4046cea1f314cfb22d03a3a29f59c117
Amit Ray <amitray@lawsikho.in>
2026-04-04 15:29:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ st.set_page_config(
     page_title="Analytics Dashboard — LawSikho",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 def get_bq_client():
